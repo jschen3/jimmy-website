434dd29c6d09f8f6f93ce6b22007b74dc57d05e0
Jimmy Chen <jimmy@Jimmys-MacBook-Pro.local>
2019-09-01 16:39:14 -0700

    adding draft of website vertical card container

--- src/components/cardcontainer/vertical/vertical_card_container.js
+++ src/components/cardcontainer/vertical/vertical_card_container.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import 'vertical_card_container.css'
+import listReactFiles from 'list-react-files'
 export class Card extends React.Component {
     constructor(props) {
+        super(props);
+        months = ["january", "february","march","april","may","june","july","august","september","octover","november","december"];
+        today = new Date();
+        todayMonth = today.getMonth();
+        firstMonth = months[todayMonth-2];
+        secondMonth = months[secondMonth-1];
+        todayMonth = months[todayMonth];
+        firstMonthFolder = "content/cards/"+firstMonth;
+        secondMonthFolder = "content/cards/"+secondMonth;
+        monthFolder = "content/cards"+todayMonth;
+
+        firstMonthFiles = listReactFiles(firstMonthFolder).then(files=>{
+            //get all files and file contents and make cards.
+        })
+
 
         /*
         fetch rows of cards.
@@ -18,16 +34,25 @@ export class Card extends React.Component {
                 <Card.Text>
                     {cardData.description}
                 </Card.Text>
-                <Button variant="primary" href="{cardData.linkUrl}">{cardData.linkText}</Button>
+                <Button variant="primary" onClick={()=> this.handleClick(cardData.linkUrl)}>{cardData.linkText}</Button>
             </Card.Body>
         </Card>
         );
+    }
+    handleClick(linkUrl){
+        window.location.href=linkUrl;
     }
     render() {
         /*
         Create a 4 by 3 row of cards.
         map of all the cards. 
         */
-        return ();
+        return (
+            <div>
+                <div className="card-row">
+                    
+                </div>
+            </div>
+        );
     }
 }
