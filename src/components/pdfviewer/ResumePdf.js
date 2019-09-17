f61530b5125d1e3f02ce59a969f1641ceba7c95e
Jimmy Chen <jimmy@Jimmys-MacBook-Pro.local>
2019-09-16 21:16:21 -0700

    i don't know why react pdf is not working

--- src/components/pdfviewer/ResumePdf.js
+++ src/components/pdfviewer/ResumePdf.js
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
- 
-export class ResumePdf extends React.Component {
+
+
+import pdfFile from './Resume.pdf';
+
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+};
+
+export class ResumePdf extends Component {
   state = {
+    file: pdfFile,
     numPages: null,
-    pageNumber: 1,
   }
- 
+
+  onFileChange = (event) => {
+    this.setState({
+      file: event.target.files[0],
+    });
+  }
+
   onDocumentLoadSuccess = ({ numPages }) => {
     this.setState({ numPages });
   }
- 
+
   render() {
-    const { pageNumber, numPages } = this.state;
- 
+    const { file, numPages } = this.state;
+
     return (
-      <div>
-        <Document
-          file="content/pdfs/Resume.pdf"
-          onLoadSuccess={this.onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <p>Page {pageNumber} of {numPages}</p>
+      <div className="Example">
+        <div className="Example__container">
+          <div className="Example__container__load">
+          </div>
+          <div className="Example__container__document">
+            <Document
+              file={file}
+              onLoadSuccess={this.onDocumentLoadSuccess}
+              options={options}
+            >
+              {
+                Array.from(
+                  new Array(numPages),
+                  (el, index) => (
+                    <Page
+                      key={`page_${index + 1}`}
+                      pageNumber={index + 1}
+                    />
+                  ),
+                )
+              }
+            </Document>
+          </div>
+        </div>
       </div>
     );
   }
