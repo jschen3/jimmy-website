0cf92fc72f7032da8b13e5e03f89ffc0ec437a06
Jimmy Chen <jimmy@Jimmys-MacBook-Pro.local>
2019-09-19 17:52:47 -0700

    finished draft of website

--- src/components/customnavbar/customnavbar.js
+++ src/components/customnavbar/customnavbar.js
@@ -3,9 +3,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import './customnavbar.css'
 export class CustomNavbar extends React.Component {
-    constructor(props){
-        super(props);
-    }
+    
     render(){
         return (<Navbar bg="dark" variant="dark">
             <Navbar.Brand href="/">Jimmy Chen</Navbar.Brand>
