4449361e2ba516b0a60ea47e84a6e937dcb1b4d4
Jimmy Chen <jimmy@jimmys-mbp.lan>
2019-09-26 17:49:31 -0600

    commented out musicalify

--- src/components/customnavbar/customnavbar.js
+++ src/components/customnavbar/customnavbar.js
@@ -9,9 +9,9 @@ export class CustomNavbar extends React.Component {
             <Navbar.Brand href="/">Jimmy Chen</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="/" className="nav-link">Home</Nav.Link>
+                <Nav.Link href="/imageportfolio" className="nav-link">Image Portfolio</Nav.Link> 
                 <Nav.Link href="/aboutme" className="nav-link">About Me</Nav.Link>
-                <Nav.Link href="/imageportfolio" className="nav-link">Image Portfolio</Nav.Link>
-                <Nav.Link href="/musicalify" className="nav-link">Musicalify</Nav.Link>
+                {/* <Nav.Link href="/musicalify" className="nav-link">Musicalify</Nav.Link> */}
             </Nav>
         </Navbar>);
     }
